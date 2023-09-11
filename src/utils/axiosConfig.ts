a4751acbb9105ece2eb39aa89c2c3c0540a54dbf
ChieduEgbumoekiJr-Dev <chiedu75@gmail.com>
2023-09-11 21:27:51 +0300

    feat: require jwt token

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -4,6 +4,6 @@ import { baseUrl } from "constants/endpoints";
 export const axiosBase = axios.create({
   baseURL: `${baseUrl}`,
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqYWFnZnhqc2JxZXlkeWlwbnVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMwNTI5MDMsImV4cCI6MjAwODYyODkwM30.Yo0ST6aywLuiN0p3qtZCBwD5gLQztzwpliyiU-9aXsg`,
   },
 });
